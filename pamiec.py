86c0d263b3fec893ef1f4692b74d22f36f677c4b
Michał <mtrela2222@gmail.com>
2025-12-28 16:32:27 +0100

    Dodanie klasy pamieci w pamiec.py i metod

---
+++ pamiec.py
@@ -0,0 +1,22 @@
+import json
+
+class Pamiec:
+    def __init__(self,sciezka):
+        self.sciezka = sciezka
+        self.dane = []
+        self.odczyt()
+
+    def odczyt(self):
+        try:
+            with open(self.sciezka,"r")as file:
+                self.dane = json.load(file)
+        except FileNotFoundError:
+            self.dane = []
+
+    def zapis(self, nowy_wpis):
+        self.dane.append(nowy_wpis)
+        try:
+            with open(self.sciezka, "w") as file:
+                json.dump(self.dane, file)
+        except Exception as e:
+            print(f"Błąd zapisu: {e}")
