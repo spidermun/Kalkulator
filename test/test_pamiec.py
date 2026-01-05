400d7fd72776d43ea85d2bcfe33aa218eaf845a7
Michał <mtrela2222@gmail.com>
2026-01-05 10:47:25 +0100

    budowa testow pliku pamiec.py

--- test/test_pamiec.py
+++ test/test_pamiec.py
@@ -8,3 +8,7 @@ def test_inicjalizajca_bez_pliku(tmp_path):
     p = Pamiec(sciezka)
     assert p.dane == []
 
+def test_zapis_do_pliku(tmp_path):
+    sciezka = tmp_path / "tesotwanie.json"
+    p = Pamiec(sciezka)
+    p.zapis(nowy_wpis="")
