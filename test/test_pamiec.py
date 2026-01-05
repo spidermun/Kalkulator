ecee0ce81b3a4a0f551e60f196ef36a7f0a739c4
Michał <mtrela2222@gmail.com>
2026-01-05 10:36:27 +0100

    budowa testow pliku pamiec.py

--- test/test_pamiec.py
+++ test/test_pamiec.py
@@ -1,10 +1,13 @@
+import json
 import pytest
 from projekt.pamiec import Pamiec
+
+p = Pamiec()
 
 def test_inicjalizajca_bez_pliku(tmp_path):
     sciezka = tmp_path / "tesotwanie.json"
 
 def test_odczyt():
-    pass
-
+    with  pytest.raises(FileNotFoundError, match=""):
+        p.odczyt()
 
