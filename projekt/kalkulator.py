d0ef4424015ed857532b19ba9b845c532b32da48
Michał <mtrela2222@gmail.com>
2026-01-08 15:19:13 +0100

    poprawka

--- projekt/kalkulator.py
+++ projekt/kalkulator.py
@@ -1,9 +1,12 @@
-
 from projekt.operacje import pobierz_operacje, Operacje
+
 
 class Kalkulator:
     def __init__(self) -> None:
-        self._operacje: dict[str, Operacje] = {op.symbol: op for op in pobierz_operacje()}
+        self._operacje: dict[str, Operacje] = {}
+        for op in pobierz_operacje():
+            symbol = op.symbol
+            self._operacje[symbol] = op
 
     def symbole(self) -> list[str]:
         return list(self._operacje.keys())
