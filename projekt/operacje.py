5829eaa9e77d008b8ba1bdb5f95d1bf6a2fd5118
Michał <mtrela2222@gmail.com>
2026-01-08 14:30:22 +0100

    dodanie klas operacji matematycznych oraz interfejsu Operacje

--- projekt/operacje.py
+++ projekt/operacje.py
@@ -1,8 +1,41 @@
+from typing import Protocol
 
-class Operacje():
+class Operacje(Protocol):
     symbol = str
     name = str
 
+    def wykonanie(self, a:float ,b:float ) -> float: ...
+
+
 class Dodawanie:
+    symbol = "+"
+    name = "dodawanie"
 
+    def wykonanie(self, a: float, b: float) -> float:
+        return a + b
 
+class Odjmowanie:
+    symbol = "-"
+    name = "odjmowanie"
+
+    def wykonanie(self, a: float, b: float) -> float:
+        return a - b
+
+class Mnozenie:
+    symbol = "*"
+    name = "mnozenie"
+
+    def wykonanie(self, a: float, b: float) -> float:
+        return a * b
+
+class Dzielenie:
+    symbol = "/"
+    name = "dzielenie"
+
+    def wykonanie(self, a: float, b: float) -> float:
+        if b == 0:
+            raise ValueError("Nie mozna dzielic przez zero")
+        return a / b
+
+def pobierz_operacje() -> list[Operacje]:
+    return [Dodawanie(),Odjmowanie(),Mnozenie(),Dzielenie()]
